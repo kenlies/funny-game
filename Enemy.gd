d9218cba6b5ba5e7f38d91e9138f228ee85016fe
Aleksander Otsala <aotsala@c1r1p6.hive.fi>
2024-01-25 05:28:39 +0200

    stuf

--- Enemy.gd
+++ Enemy.gd
@@ -1,19 +1,26 @@
 extends KinematicBody2D
 
 var player
-var speed = 100
+var stunned = false
+var jump = false
+export var speed = 50
 onready var _animated_sprite = $AnimatedSprite
-	
+onready var _jump_timer = $JumpTimer
+onready var _stun_timer = $StunTimer
+onready var _rigidbody2d = $RigidBody2D
+
+
 func _process(delta):
 	# if we are detected, start the chase
+	if stunned == true or jump:
+		return 
 	if player:
 		var direction = (player.global_position - global_position).normalized()
 		# flip the sprite based on left or right direction on the x axis
 		if direction.x > 0:
-			_animated_sprite.flip_h = true
-		else:
 			_animated_sprite.flip_h = false
-		_animated_sprite.play("default")
+		else:
+			_animated_sprite.flip_h = true
 		# move the enemy towards the player times speed
 		move_and_slide(direction * speed)
 
@@ -22,3 +29,17 @@ func _on_PlayerDetection_body_entered(body):
 	if body.name == "Player":
 		player = body
 		
+
+
+func _on_JumpTimer_timeout():
+	if not jump:
+		jump = true
+		_animated_sprite.set_frame(0)
+	else:
+		jump = false
+		_animated_sprite.set_frame(1)
+
+
+func _on_StunTimer_timeout():
+	stunned = false
+
--- Player.gd
+++ Player.gd
@@ -1,21 +1,32 @@
 extends KinematicBody2D
 
-export (int) var speed = 300
+export var speed = 200
+export var max_enemy_cout = 100
+var enemy_count = 0
 onready var _animated_sprite = $AnimatedSprite
 onready var _dashtimer = $DashTimer
 onready var _dashcooldowntimer = $DashCoolDownTimer
 var velocity = Vector2()
 var enemy = preload("res://Enemy.tscn")
 onready var _spawntimer = $SpawnTimer
+onready var _camera = $Camera2D
+onready var _area = $Area2D
 var player = null
 
 func get_input():
 	velocity = Vector2()
-	
+
+	# handling stun
+	if Input.is_action_pressed("ui_joke"):
+		for i in _area.get_overlapping_bodies():
+			if "Enemy" in i.name:
+				i.stunned = true
+				i._stun_timer.start()
+
 	# handling dash and starting timers
-	if Input.is_action_pressed("ui_space") and _dashcooldowntimer.is_stopped():
+	if Input.is_action_pressed("ui_dash") and _dashcooldowntimer.is_stopped():
 		_dashtimer.start()
-		speed = 1200
+		speed = 500
 		_dashcooldowntimer.start()
 	
 	# moving the player and playing animations
@@ -34,28 +45,30 @@ func get_input():
 	velocity = velocity.normalized() * speed
 
 func _on_DashTimer_timeout():
-	speed = 300
+	speed = 100
 
 # making the player move on every frame (if velocity is > 0)
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 
-func getPosOutsideCamera():
-	var randx
-	var randy
-	var distance_outside_screen = 100
-	var screensize
-	
-
 # spawn an enemy
 func _on_SpawnTimer_timeout():
-	if player == null:
-		player = get_parent().get_node("Player")
-	var viewport_rect = get_viewport().size
-	print(viewport_rect)
-	var spawn_position = Vector2(rand_range(-viewport_rect.x, 2 * viewport_rect.x), rand_range(-viewport_rect.y, 2 * viewport_rect.y))
-	spawn_position = player.global_position + spawn_position
-	var enemy_instance = enemy.instance()
-	enemy_instance.position = spawn_position
-	get_parent().add_child(enemy_instance)
+	if enemy_count < max_enemy_cout:
+		if player == null:
+			player = get_parent().get_node("Player")
+		var camera_center = _camera.get_camera_screen_center()
+		# below has a bug that it doesn't spawn directly up, down, left or right
+		var spawn_position = Vector2(choose_range(rand_range(camera_center.x - 200, camera_center.x - 100), rand_range(camera_center.x + 100, camera_center.x + 200)),
+									choose_range(rand_range(camera_center.y - 200, camera_center.y - 100), rand_range(camera_center.y + 100, camera_center.y + 200)))
+		
+		var enemy_instance = enemy.instance()
+		enemy_instance.position = spawn_position
+		get_parent().add_child(enemy_instance)
+		enemy_count += 1
+	
+func choose_range(val1, val2):
+	if randf() < 0.5:
+		return val1
+	return val2
+
